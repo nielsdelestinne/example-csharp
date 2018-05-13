34b94fa824a4d524326dd996d6c75551926d2dfa
nielsdelestinne <nielsdelestinne@gmail.com>
2018-05-13 18:35:26 +0200

    experimenting with some LINQ

---
+++ InheritanceAndOthers/Human.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndOthers
+{
+    class Human
+    {
+
+        public virtual void DoSomething()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
--- InheritanceAndOthers/Program.cs
+++ InheritanceAndOthers/Program.cs
@@ -21,6 +21,10 @@ namespace InheritanceAndOthers
                 bob
             };
 
+            int howMany = employees?.Count ?? 0;
+
+            int ?test = employees?.Count;
+
             foreach (Employee employee in employees)
             {
                 Console.WriteLine(employee);
@@ -29,6 +33,52 @@ namespace InheritanceAndOthers
             for (int i = 0; i < employees.Count; i++)
             {
                 Console.WriteLine(employees[i]);
+            }
+
+            List<int> listofInt = new List<int> {
+                1,
+                5,
+                8,
+                10,
+                15,
+                5,
+                6,
+                7
+            };
+
+            // or var smallerNumbers (less boilerplate)
+            IEnumerable<int> smallerNumbers = from num in listofInt
+                                 where num < 8
+                                 select num;
+
+            foreach( int number in smallerNumbers) {
+                Console.WriteLine($"small: {number}");
+            }
+
+            Console.WriteLine("\n--------------\n");
+
+            var smallerNumbers2 = listofInt
+                .Where(n => n < 8)
+                .Select(n => n);
+
+            foreach (int number in smallerNumbers)
+            {
+                Console.WriteLine($"small2: {number}");
+            }
+
+            Console.WriteLine("\n--------------\n");
+
+            var methods = from method in typeof(int).GetMethods()
+                          orderby method.Name
+                          group method by method.Name into groups
+                          select new
+                          {
+                              MethodName = groups.Key,
+                              MethodOverloads = groups.Count()
+                          };
+            foreach (var method in methods)
+            {
+                Console.WriteLine($"{method.MethodName} - {method.MethodOverloads}");
             }
         }
     }
